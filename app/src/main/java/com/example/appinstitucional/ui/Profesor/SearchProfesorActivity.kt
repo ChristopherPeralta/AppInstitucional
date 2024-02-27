3984184d99e47ce31b309fe0f364050c861916d3
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-02-27 09:26:21 -0500

    Profesor-Busqueda

---
+++ app/src/main/java/com/example/appinstitucional/ui/Profesor/SearchProfesorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appinstitucional.ui.Profesor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appinstitucional.R
+
+class SearchProfesorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_profesor)
+    }
+}
