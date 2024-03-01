d8a489f0edb513042441dbbf021dc327d42c79a8
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-03-01 11:21:12 -0500

    RolAdmin
    Clases

---
+++ app/src/main/java/com/example/appinstitucional/ui/Administrador/NivelAdmin.kt
@@ -0,0 +1,12 @@
+package com.example.appinstitucional.ui.Administrador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appinstitucional.R
+
+class NivelAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_nivel)
+    }
+}
