13b96352161a29b9f6d12c7d465ae78e58eeb957
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-03-01 11:21:28 -0500

    RolAlumno
    Pantallas

---
+++ app/src/main/java/com/example/appinstitucional/ui/Alumno/AlumnoCursoAsistencia.kt
@@ -0,0 +1,12 @@
+package com.example.appinstitucional.ui.Alumno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appinstitucional.R
+
+class AlumnoCursoAsistencia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alumno_curso_asistencia)
+    }
+}
