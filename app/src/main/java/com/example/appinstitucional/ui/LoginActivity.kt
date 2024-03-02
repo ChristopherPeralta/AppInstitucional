2ce80fde904fc6c1066b609e3284188df4e7453d
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-03-02 00:26:45 -0500

    LoginNavegacion

--- app/src/main/java/com/example/appinstitucional/ui/LoginActivity.kt
+++ app/src/main/java/com/example/appinstitucional/ui/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appinstitucional.R
 import com.example.appinstitucional.ui.Administrador.AdministradorActivity
@@ -17,6 +18,18 @@ class LoginActivity : AppCompatActivity() {
 
         val email = findViewById<EditText>(R.id.email)
         val password = findViewById<EditText>(R.id.password)
+
+        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
+        btnIngresar.setOnClickListener {
+            val intent = Intent(this, AdministradorActivity::class.java)
+            startActivity(intent)
+        }
+
+        val olvideContrasena = findViewById<TextView>(R.id.olvidasteContrasena)
+        olvideContrasena.setOnClickListener {
+            val intent = Intent(this, OlvideContrasena::class.java)
+            startActivity(intent)
+        }
 
     }
 }
