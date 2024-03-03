02c8932ff62580bdd089f89047d02ee00617933a
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-03-03 06:51:15 -0500

    StringsAdminHome

--- app/src/main/java/com/example/appinstitucional/ui/Administrador/AdministradorActivity.kt
+++ app/src/main/java/com/example/appinstitucional/ui/Administrador/AdministradorActivity.kt
@@ -1,12 +1,62 @@
 package com.example.appinstitucional.ui.Administrador
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import com.example.appinstitucional.R
+import com.example.appinstitucional.ui.Profesor.ProfesorActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class AdministradorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_administrador)
+
+        val btnNivel = findViewById<Button>(R.id.btnNivel)
+        btnNivel.setOnClickListener {
+            val intent = Intent(this, NivelAdmin::class.java)
+            startActivity(intent)
+        }
+
+        val btnGrado = findViewById<Button>(R.id.btnGrado)
+        btnGrado.setOnClickListener {
+            val intent = Intent(this, GradoAdmin::class.java)
+            startActivity(intent)
+        }
+
+        val btnSeccion = findViewById<Button>(R.id.btnSeccion)
+        btnSeccion.setOnClickListener {
+            val intent = Intent(this, SeccionAdmin::class.java)
+            startActivity(intent)
+        }
+
+        OnNavigationItemSelectedListener()
+    }
+
+    private fun OnNavigationItemSelectedListener() {
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.iconCursos -> {
+                    val intent = Intent(this, AdministradorActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.iconProfesor -> {
+                    val intent = Intent(this, ProfesorAdmin::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.iconAlumno -> {
+                    val intent = Intent(this, AlumnoAdmin::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
