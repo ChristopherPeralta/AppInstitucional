01d8b48b9de2f4db85155783c44de9b522b26592
ChristopherPeralta <cperaltapenaloza@gmail.com>
2024-03-12 19:02:27 -0500

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.cardview:cardview:1.0.0")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 }
